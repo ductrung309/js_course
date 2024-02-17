b4a3bdec2f7270b64ee71df2646a408a1768b202
trungne <anhlatrung5c@gmail.com>
2024-02-18 00:26:46 +0700

    toi nay toi day thoi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 import { engine } from "express-handlebars";
 
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.static("./src/public"));
+
+//HTTPS LOGGER
 app.use(morgan("combined"));
 
+//Templates engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
