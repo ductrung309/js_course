db837a5254b1026abf9e9d33919ce43cf01d3528
trungne <anhlatrung5c@gmail.com>
2024-02-24 16:59:22 +0700

    push server

--- src/config/mail.js
+++ src/config/mail.js
@@ -16,7 +16,7 @@ export function mail(sub, code) {
 
   const mailOptions = {
     from: "anhlatrung5c@gmail.com",
-    to: "longthancongviec1@gmail.com",
+    to: "trungchoido11@gmail.com",
     subject: sub,
     html: `<body style="text-align: center; margin: 0 auto; width: 490px; border: 1px solid black"><h1 style="text-align: center">Mã Đăng Nhập</h1><h3 style="text-align: center; color: grey; margin-top: 50px">Đây là mã đăng nhập phê duyệt của bạn</h3><h2 style = "text-align: center; color: red">${code}</h2><h3 style="text-align: center; font-family: Arial, Helvetica, sans-serif">Nếu bạn không phải là người gửi yêu cầu này, hãy đổi mật khẩu tài khoản ngay lập tức để tránh việc bị truy cập trái phép. Đọc Bảo Mật Tài Khoản để biết các mẹo về mật khẩu có tính bảo mật cao.</h3></body>`,
   };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
 import { mail } from "./config/mail.js";
+import XMLHttpRequest from "xhr2";
 
 const app = express();
 const port = process.env.port || 3000;
@@ -13,7 +14,6 @@ app.use(express.static("./src/public"));
 app.use(morgan("combined"));
 
 //templates engine
-
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
@@ -24,8 +24,13 @@ app.get("/", (req, res) => {
 });
 
 //send code
-mail("Nhớ cổ wa đi à", Math.floor(Math.random() * 1000000));
+// mail("Nhớ cổ wa đi à", Math.floor(Math.random() * 1000000));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://d25e-123-16-75-216.ngrok-free.app", true);
+xhr.setRequestHeader("ngrok-skip-browser-warning", "true");
+xhr.send();
